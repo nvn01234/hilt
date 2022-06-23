d4f0627160e0e7ba4404b23dc6e28affcfedf11d
sondn98 <son.dn@teko.vn>
2022-06-23 15:25:25 +0700

    [storage/hadooop] Push image after building

--- storage/hadoop/components/nodemanager/build.sh
+++ storage/hadoop/components/nodemanager/build.sh
@@ -1,3 +1,4 @@
 export HADOOP_NODEMANAGER_VERSION=0.1.0
 export HADOOP_VERSION=3.3.1
 docker build --tag sondn1/hadoop-nodemanager:$HADOOP_NODEMANAGER_VERSION-hadoop_$HADOOP_VERSION .
+docker push sondn1/hadoop-nodemanager:$HADOOP_NODEMANAGER_VERSION-hadoop_$HADOOP_VERSION
