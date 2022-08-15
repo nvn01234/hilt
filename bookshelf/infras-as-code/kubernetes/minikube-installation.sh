960d0215d2f0e919cab3cb441a76bbbdfb1ae6f7
sondn <son.dn@teko.vn>
2022-08-15 10:36:25 +0700

    [helm] Change minikube driver to virtualbox

--- bookshelf/infras-as-code/kubernetes/minikube-installation.sh
+++ bookshelf/infras-as-code/kubernetes/minikube-installation.sh
@@ -10,17 +10,31 @@ curl -Lo /tmp/minikube $DOWNLOAD_LINK
 echo $SUDO_PASSWORD | sudo -S install /tmp/minikube /usr/local/bin/minikube
 
 echo "******************************* WARNING ********************************"
-echo "The Docker driver allows you to install Kubernetes into an existing Docker install. On Linux, this does not require virtualization to be enabled."
+echo "VirtualBox is minikube’s original driver. It may not provide the fastest start-up time, but it is the most stable driver available for users of Microsoft Windows Home"
 echo "The following requirements must be fulfilled:"
-echo "- Docker 20.10 or higher"
-echo "- Cgroup v2 delegation"
-echo "- Kernel 5.11 or later (5.13 or later is recommended when SELinux is enabled)"
+echo "- VirtualBox 5.2 or higher"
 read -p "If these requirements has been fulfilled, press ENTER to continue. Else, press any button to cancel" CONTINUE
+
+# echo "******************************* WARNING ********************************"
+# echo "The Docker driver allows you to install Kubernetes into an existing Docker install. On Linux, this does not require virtualization to be enabled."
+# echo "The following requirements must be fulfilled:"
+# echo "- Docker 20.10 or higher"
+# echo "- Cgroup v2 delegation"
+# echo "- Kernel 5.11 or later (5.13 or later is recommended when SELinux is enabled)"
+# read -p "If these requirements has been fulfilled, press ENTER to continue. Else, press any button to cancel" CONTINUE
 
 if [ ${#CONTINUE} -ne 0 ]; then
   echo "OK, bye"
   exit 0
 fi
 
-dockerd-rootless-setuptool.sh install -f
-docker context use rootless
+minikube start --driver=virtualbox \
+               --mount \
+               --mount-string="/data1/minikube:/data1" \
+               --container-runtime=containerd \
+               --memory 8192 --cpus 4 \
+               --nodes 1
+
+# dockerd-rootless-setuptool.sh install -f
+# docker context use rootless
+
