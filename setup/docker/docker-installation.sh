54e7965d99bf025cb31fac6a2c9fc4440c62a9aa
sondn98 <dongocson.work@gmail.com>
2022-10-29 13:10:55 +0700

    [setup/docker] Edit docker setup

--- setup/docker/docker-installation.sh
+++ setup/docker/docker-installation.sh
@@ -8,17 +8,21 @@ read SUDO_PASSWORD
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS
 echo $SUDO_PASSWORD | sudo -S apt remove docker docker-engine docker.io containerd runc
 
-# Add Docker’s official GPG key
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release
 
-# Use the following command to set up the repository
+# Add Docker’s official GPG key
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
+# Use the following command to set up the repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 # Install docker engine
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Manage docker as a non-root user. Reboot or re-login may be required after this installation
 sudo groupadd docker
