731c9b7562e942aa2f88619598a7f87285ff5876
sondn98 <dongocson.work@gmail.com>
2023-04-11 22:51:13 +0700

    [docker/computation/nifi] Generate mock data for postgresql

--- docker/databases/postgresql/sample/dvdrental.sh
+++ docker/databases/postgresql/sample/dvdrental.sh
@@ -2,5 +2,5 @@
 HERE="$(dirname "${BASH_SOURCE[0]}")"
 POSTGRES_USER="${POSTGRES_USER:-postgres}"
 
-createdb   --username=${POSTGRES_USER} --no-password    dvdrental
-pg_restore --username=${POSTGRES_USER} --no-password -d dvdrental $HERE/dvdrental.tar
+PGPASSWORD=SuperSecr3t createdb   --username=${POSTGRES_USER}    dvdrental
+PGPASSWORD=SuperSecr3t pg_restore --username=${POSTGRES_USER} -d dvdrental $HERE/dvdrental.tar
